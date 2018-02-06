bc6b49e62ae9753f8ce57e38d2009ab1b12127aa
Ebony Plummer <ebony.plummer@gmail.com>
2018-02-05 23:45:01 -0500

    Added custom markers and a legend to the logic.js file.

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
 //Create the function to determine marker size base on eartkquake magnitude.
-function markerSize(magnitude) {
-	return feature.properties.mag * 10;
+function getColor(d) {
+    return d > 5  ? '#E31A1C' :
+           d > 4  ? '#FC4E2A' :
+           d > 3   ? '#FD8D3C' :
+           d > 2   ? '#FEB24C' :
+           d > 1   ? '#FED976' :
+                      '#FFEDA0';
 }
 
 
@@ -21,25 +26,38 @@ function createFeatures(earthquakeData) {
 	}
 
 	var earthquakes = L.geoJSON(earthquakeData, {
-		onEachFeature: onEachFeature
+		onEachFeature: onEachFeature, 
+		pointToLayer: pointToLayer
 	});
 
 	//Sending the earthquakes layer to the createMap function.
 	createMap(earthquakes);
-}
 
-var magnitudeMarkers = []
-//Loop through the earthquake data and creat markers based on earthquake magnitude.
-for (var i = 0; data.length; i++) {
-	magnitudeMarkers.push(
-		L.circle(data[i].features.properties.coordinates, {
-			stroke:false,
-			fillOpacity: 0.75,
-			color:"blue",
-			fillColor:"blue",
-			radius: markerSize(earthquakeData[i].features.properties.mag)
-	})
-  );
+
+    //var magnitudeMarkers = []
+   //Loop through the earthquake data and creat markers based on earthquake magnitude.
+   //for (var i = 0; earthquakeData.length; i++) {
+   //var circleOptions = {
+			//stroke:false,
+			//fillOpacity: 0.75,
+			//color:"blue",
+			//fillColor:"blue",
+			//radius: feature.properties.mag * 50000
+
+  		
+   	function pointToLayer(feature,latlng){
+   		return new L.circle(latlng,{
+   			stroke:false,
+			fillOpacity: 0.7,
+			//color:"blue",
+			fillColor:getColor(feature.properties.mag),
+			radius: feature.properties.mag * 50000
+
+
+   		})
+
+   	}
+		
 }
 
 function createMap(earthquakes) {
@@ -74,4 +92,26 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps,overlayMaps,{
     	collapse: false
     }).addTo(myMap);
- }
+    
+    //Adding a legend to the map.
+    var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [1,2,3,4,5],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+ };  
